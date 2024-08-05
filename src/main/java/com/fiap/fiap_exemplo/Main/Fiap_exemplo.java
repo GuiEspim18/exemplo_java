17d3f5891f1b937151ebed9cdb0056f9dec11bc2
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-05 10:34:23 -0300

    init

---
+++ src/main/java/com/fiap/fiap_exemplo/Main/Fiap_exemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fiap.fiap_exemplo.Main;
+
+/**
+ *
+ * @author labsfiap
+ */
+public class Fiap_exemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
